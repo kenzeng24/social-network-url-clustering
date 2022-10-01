b5f7f6d788ac0dcb34818e9a1afd4d79089ac61f
Frances Yuan <francesy@119.134.182.163.naturalwireless.com>
2022-10-01 12:00:32 -0400

    add run_every15min

--- src/twitter_api/users.py
+++ src/twitter_api/users.py
@@ -7,6 +7,10 @@ from tqdm import tqdm
 
 # import botometer
 
+import json
+from datetime import datetime
+from time import sleep
+
 
 # TODO: are there other features we can collect 
 USER_FEATURES = [
@@ -17,6 +21,13 @@ USER_FEATURES = [
     'friends_count',
     'protected'
 ] 
+
+# all features (excluding no longer supported (deprecated) attributes)
+USER_FEATURES_FULL = ['id', 'id_str', 'name', 'screen_name', 
+                    'location', 'url', 'description', 'protected', 'verified', 
+                    'followers_count', 'friends_count', 'listed_count', 'favourites_count', 'statuses_count', 
+                    'created_at', 'profile_banner_url', 'profile_image_url_https', 
+                    'default_profile', 'default_profile_image', 'withheld_in_countries']
 
 
 def create_tweepy_auth(twitter_app_auth):
@@ -102,8 +113,36 @@ class ProfileScraper:
                 df.to_csv(save_file, index=False, 
                           mode='w' if i == 0 else 'a', 
                           header=(i==0))
-            # add current row to existing dataframe
-            cumulative_df = df if i == 0 else cumulative_df.append(df)
+            # # add current row to existing dataframe
+            # cumulative_df = df if i == 0 else cumulative_df.append(df)
 
-        return cumulative_df 
+        # return cumulative_df 
+
+    def run_every15min(self, users, features=USER_FEATURES, save_file=None, num_requests=900):
+        """
+        
+        Limit the number of requests for every 15 minutes 
+        until all user profiles are scraped
+        """
+        print(f'Number of users: {len(users)}')
+        print(f'Time needed: {len(users)//num_requests*15} min')
+
+        i = 0
+        while True:
+            now = datetime.now()
+            dt_string = now.strftime("%Y/%m/%d %H:%M:%S")
+            print(dt_string)
+
+            # save as a new file every 15 minutes
+            sub_save_file = save_file.split('.')
+            sub_save_file[-2] += ('_' + str(i+1))
+
+            sub_users_list = users[i*num_requests: (i+1)*num_requests]
+            self.run(sub_users_list, features, '.'.join(sub_save_file))
+
+            i += 1
+            if i * num_requests >= len(users):
+                return # finish
+
+            sleep(60*15)  # Wait for 15 minutes
   

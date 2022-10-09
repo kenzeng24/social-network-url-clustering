52a5220216e7b5f4b5ef70d7dcb29ee2de27125e
Anthony Chen <52572060+achen004@users.noreply.github.com>
2022-10-09 13:59:21 -0400

    specified column to use

--- src/preprocessing/textstat.py
+++ src/preprocessing/textstat.py
@@ -10,10 +10,10 @@ def text_score_generator(filename):
 	macld_df=pd.read_json(macld)
 	
 	#Flesh Reading Ease Score
-	macld_df['FLRE_scre']=macld_df.apply(lambda x: textstat.flesch_reading_ease(x))
+	macld_df['FLRE_scre']=macld_df['agg_text'].apply(lambda x: textstat.flesch_reading_ease(x))
 
 	#Grade level agg. test of text
-	macld_df['grade_scre']=macld_df.apply(lambda x: textstat.text_standard(x))	
+	macld_df['grade_scre']=macld_df['agg_text'].apply(lambda x: textstat.text_standard(x))	
 
 	return macld_df 
 		

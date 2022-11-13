df9ca236c9d3e501ad9a86fe6fd8f4c31db0fb33
Ken Zeng <31870042+kenzeng24@users.noreply.github.com>
2022-11-13 12:52:57 -0500

    added new data loading

--- src/data_loading/training_data.py
+++ src/data_loading/training_data.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd 
-import scipy.sparse as sparse
-
-from src.data_loading.interactions import ROOT, METADATA_FILE
+import numpy as np 
+from scipy.sparse import load_npz, vstack
+from src.data_loading.interactions import ROOT, METADATA_FILE, get_metadata
 from src.preprocessing.vectorize_text import TFIDF_MATRIX
 
 MBFC_LABELS_PATH = os.path.join(ROOT, 'data/Classification_Labels.csv')
@@ -26,6 +26,25 @@ def get_tfidf_dataset(
     y = filtered_nonnull['Credibility_Rank'] # for now 
     urls = metadata[['url', 'id_hash256']].iloc[filtered_nonnull.index]
     return X,y,urls
+
+
+def get_balanced_tfidf_data():
+
+    # get the tfidf vectors from original dataset 
+    tfidf_matrix_file =  os.path.join(ROOT, 'data/balanced-dataset-tfidf-matrix-original.npz')
+    X, y, urls = get_tfidf_dataset(tfidf_matrix_file=tfidf_matrix_file) 
+
+    # get metadata and tfidf matrix from new dataset 
+    metadata_hc = get_metadata(filename=os.path.join(ROOT, 'data/capstone_url_metadata_hc.json'))
+    X_hc = load_npz(os.path.join(ROOT, 'data/balanced-dataset-tfidf-matrix-hc.npz'))
+    y_hc = np.ones(shape=X_hc.shape[0]) # all urls in this sample are not suspiciious
+    urls_hc = metadata_hc[['url', 'id_hash256']]
+
+    # combine the outputs from both data sources 
+    X_combined = vstack([X, X_hc])
+    y_combined = np.hstack([y.values, y_hc])
+    urls_combined =  pd.concat([urls, urls_hc])
+    return X_combined, y_combined, urls_combined
 
 
 def get_unabeled_urls(

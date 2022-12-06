782be581322bb260e902dcbe9fbd49724d5fd2ef
Anthony_Chen <ac8480@nyu.edu>
2022-12-05 21:11:53 -0500

    updated cluster function

--- src/clustering/cluster_scres.py
+++ src/clustering/cluster_scres.py
@@ -1,15 +1,31 @@
 import pandas as pd
 import numpy as np
 
+cluster_files = os.path.join(root, 'data/cluster_generated_reduced.json')
+suspicion_scores = os.path.join(root, 'data/20221205-suspicion-scores.csv')
+
+ROOT = 'drive/MyDrive/CDS_Capstone_2022_Fall'
+metadata = pd.read_json(os.path.join(ROOT, 'data/capstone_url_metadata.json'))
+metadata_hc = pd.read_json(os.path.join(ROOT, 'data/capstone_url_metadata_hc.json'))
+
+og_meta=metadata[['url', 'id_hash256']]
+
+merged_urls_meta=pd.concat([og_meta, metadata_hc], axis=0)
+merged_urls_meta.head()
+
+#input cluster_files, suspicion_scores
 def get_cluster_scores(cluster_json, scores):
     """takes in json file and outputs a dataframe of nan clusters and scored clusters"""
     cluster_df=pd.read_json(cluster_json)
     ss_df=pd.read_csv(scores)
+    #merge original urls with scores
+    urls_ss=pd.merge(ss_df, merged_urls_meta, how='inner') 
     avg_cluster_susscores={}
     for i in range(len(updated_cluster.columns)):
       ith_cluster=updated_cluster.iloc[:,i].to_frame()
       ith_cluster=ith_cluster.rename(columns={i:'url'})
-      merged_urls=pd.merge(ith_cluster, ss_df, how='inner')
+      #merge each cluster with its scores
+      merged_urls=pd.merge(ith_cluster, urls_ss, how='inner')
       avg_sus_score=merged_urls.score.mean()
       avg_cluster_susscores[i]=avg_sus_score
     acs=pd.DataFrame(avg_cluster_susscores, index=[0])
